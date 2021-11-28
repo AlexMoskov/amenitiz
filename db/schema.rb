d1e1dfa6d90b5df8134f315182eb8862052bfbfa
Alex Moskovka <oleksandr.moskovka@versapay.com>
2021-11-28 22:53:17 +0200

    Added small changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_27_233904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.text "product_code"
     t.text "name"
